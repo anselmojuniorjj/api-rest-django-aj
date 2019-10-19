1dd6e0d713a4b9c1d236a684fba94563338cee53
Anselmo Junior <anselmo.junior@globalhitss.com.br>
2019-10-19 20:06:39 -0300

    url token

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
+
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
@@ -28,7 +30,7 @@ from recursos.api.viewsets import RecursoViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'pontosturisticos', PontoTuristicoViewSet)
+router.register(r'pontosturisticos', PontoTuristicoViewSet, base_name='PontoTuristico')
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
@@ -38,4 +40,5 @@ router.register(r'recursos', RecursoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
